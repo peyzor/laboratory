f3464973da28fc1958bca96b0e7642a9db7521d3
Peyman <hassani.peyman627@gmail.com>
2024-10-10 10:10:21 +0330

    Add second memory usage decorator

--- mem_usage_dec.py
+++ mem_usage_dec.py
@@ -30,11 +30,31 @@ def mem_usage(func):
     return wrapper
 
 
-@mem_usage
+def measure_memory_usage(func):
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+
+        result = func(*args, **kwargs)
+
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics("lineno")
+
+        print(f"Memory usage of {func.__name__}:")
+        for stat in top_stats[:5]:
+            print(stat)
+
+        return result
+
+    return wrapper
+
+
+@measure_memory_usage
 def app():
     lt = []
     for i in range(0, 100000):
         lt.append(i)
+
+    return lt
 
 
 if __name__ == '__main__':
