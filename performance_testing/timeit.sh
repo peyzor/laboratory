996dac6a7f3a6e57e8ca3d1f08fa42cf2d1917ac
Peyman <hassani.peyman627@gmail.com>
2025-04-12 19:03:30 +0330

    Update timeit.sh to its original content

--- performance_testing/timeit.sh
+++ performance_testing/timeit.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 SETUP="
-import random
-def my_sort(x):
-    return sorted(x)
-
-def my_reversed_sort(x):
-    return sorted(x, reverse=True)
-
-x = [i for i in range(100, 10_000, 3)]
+x = range(10_000)
+y = range(10_000)
 "
 
-#SETUP2="
-#from itertools import chain
-#x = range(10_000)
-#y = range(10_000)
-#"
+SETUP2="
+from itertools import chain
+x = range(10_000)
+y = range(10_000)
+"
 
 echo append impl:
-python -m timeit -s "$SETUP" "random.shuffle(x)" "my_sort(x)"
+python -m timeit -s "$SETUP" "sum(list(x) + list(y))"
 
-#echo chain iterator impl:
-#python -m timeit -s "$SETUP2" "sum(chain(x, y))"
+echo chain iterator impl:
+python -m timeit -s "$SETUP2" "sum(chain(x, y))"
