39f7027543803c2eb7cccedb0b514ebb5cb8e236
Peyman <hassani.peyman627@gmail.com>
2025-04-12 17:04:51 +0330

    Add top words question

---
+++ comapnies/top_words.py
@@ -0,0 +1,65 @@
+# torob interview
+from multiprocessing import Pool
+
+from collections import defaultdict
+
+special_chars = ['"', '(', ')', '{', '}', '[', ']', '.', "s'", "'s", '#', '*']
+
+
+def clean_word(word):
+    for char in special_chars:
+        if char in word:
+            word = word.replace(char, '')
+
+    return word
+
+
+def find_top_occurrences(word_map):
+    top_occurrences = []
+    for word, count in word_map.items():
+        if len(top_occurrences) < 5:
+            top_occurrences.append((word, count))
+            continue
+
+        top_occurrences = sorted(top_occurrences, key=lambda x: x[1], reverse=True)
+        if count > top_occurrences[-1][1]:
+            top_occurrences.pop()
+            top_occurrences.append((word, count))
+
+    return top_occurrences
+
+
+def top_words(line):
+    word_map = defaultdict(int)
+    if not line:
+        return
+
+    words = line.strip().split()
+    for word in words:
+        word = clean_word(word)
+        if not word:
+            continue
+
+        word_map[word] += 1
+
+    return list(word_map.items())
+
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        with open('mytext.txt', 'r') as f:
+            results = p.map(top_words, (line for line in f))
+            # print(results)
+
+        word_map = defaultdict(int)
+        for res in results:
+            if not res:
+                continue
+
+            # print(res)
+
+            for word, count in res:
+                word_map[word] += count
+
+        final_result = find_top_occurrences(word_map)
+        print(final_result)
