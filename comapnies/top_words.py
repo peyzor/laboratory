4dde7ff7a5410d93cbf17c108b263265d96c7875
Peyman <hassani.peyman627@gmail.com>
2025-04-12 22:48:40 +0330

    Add heap implementation for top words interview question

--- comapnies/top_words.py
+++ comapnies/top_words.py
@@ -1,6 +1,6 @@
 # torob interview
 from multiprocessing import Pool
-
+import heapq
 from collections import defaultdict
 
 special_chars = ['"', '(', ')', '{', '}', '[', ']', '.', "s'", "'s", '#', '*']
@@ -14,22 +14,26 @@ def clean_word(word):
     return word
 
 
+# def find_top_occurrences(word_map):
+#     top_occurrences = []
+#     for word, count in word_map.items():
+#         if len(top_occurrences) < 5:
+#             top_occurrences.append((word, count))
+#             continue
+#
+#         top_occurrences = sorted(top_occurrences, key=lambda x: x[1], reverse=True)
+#         if count > top_occurrences[-1][1]:
+#             top_occurrences.pop()
+#             top_occurrences.append((word, count))
+
+# def find_top_occurrences(word_map):
+#     return sorted(word_map.items(), key=lambda x: x[1], reverse=True)[:5]
+
 def find_top_occurrences(word_map):
-    top_occurrences = []
-    for word, count in word_map.items():
-        if len(top_occurrences) < 5:
-            top_occurrences.append((word, count))
-            continue
-
-        top_occurrences = sorted(top_occurrences, key=lambda x: x[1], reverse=True)
-        if count > top_occurrences[-1][1]:
-            top_occurrences.pop()
-            top_occurrences.append((word, count))
-
-    return top_occurrences
+    return heapq.nlargest(5, word_map.items(), lambda x: x[1])
 
 
-def top_words(line):
+def line_words(line):
     word_map = defaultdict(int)
     if not line:
         return
@@ -48,7 +52,8 @@ def top_words(line):
 if __name__ == '__main__':
     with Pool(5) as p:
         with open('mytext.txt', 'r') as f:
-            results = p.map(top_words, (line for line in f))
+            # because we use a generator the processes will not overlap :)
+            results = p.map(line_words, (line for line in f))
             # print(results)
 
         word_map = defaultdict(int)
