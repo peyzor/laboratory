d751393c082c8c3862ecdcd4ca0a5fae511450be
Peyman <hassani.peyman627@gmail.com>
2025-05-03 14:25:37 +0330

    Add merge sort

---
+++ DSA/sorting/merge_sort.py
@@ -0,0 +1,43 @@
+def merge_sort(nums):
+    if len(nums) < 2:
+        return nums
+
+    mid = len(nums) // 2
+    l = merge_sort(nums[:mid])
+    r = merge_sort(nums[mid:])
+    return merge(l, r)
+
+
+def merge(first, second):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(first) and j < len(second):
+        if first[i] <= second[j]:
+            result.append(first[i])
+            i += 1
+        else:
+            result.append(second[j])
+            j += 1
+
+    while i < len(first):
+        result.append(first[i])
+        i += 1
+
+    while j < len(second):
+        result.append(second[j])
+        j += 1
+
+    return result
+
+
+def main():
+    nums = [4, 3, 1, 5, 61, 3, 17]
+    sorted_nums = merge_sort(nums)
+    print('nums', nums)
+    print('sorted_nums', sorted_nums)
+
+
+if __name__ == '__main__':
+    main()
