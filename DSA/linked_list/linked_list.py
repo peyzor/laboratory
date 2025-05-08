e5c06a79594dfbdf93d833f5f3dd0c80876439e8
Peyman <hassani.peyman627@gmail.com>
2025-05-08 18:45:02 +0330

    Add tail tracking

--- DSA/linked_list/linked_list.py
+++ DSA/linked_list/linked_list.py
@@ -1,8 +1,15 @@
 class LinkedList:
     def __init__(self, head):
         self.head = head
+        # if keeping track of tail is necessary
+        self.tail = None
 
     def add_to_head(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+            return
+
         node.next = self.head
         self.head = node
 
@@ -16,6 +23,7 @@ class LinkedList:
             head = head.next
 
         head.next = node
+        self.tail = node
 
     # # generator impl
     # def __iter__(self):
@@ -46,6 +54,9 @@ class Node:
     def set_next(self, node):
         self.next = node
 
+    def __str__(self):
+        return f'{self.val}'
+
 
 def main():
     n1 = Node(1)
@@ -62,12 +73,14 @@ def main():
     print('-' * 20)
     n4 = Node(4)
     ll.add_to_tail(n4)
+    print('TAIL: ', ll.tail)
     for item in ll:
         print(item.val)
 
     print('-' * 20)
     n5 = Node(5)
     ll.add_to_head(n5)
+    print('TAIL: ', ll.tail)
     for item in ll:
         print(item.val)
 
